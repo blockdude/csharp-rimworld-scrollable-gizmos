f878e0b0b4ff9981f0691be28c0b3e9ec7ef2030
Anthony Cobb <2blockdude@gmail.com>
2022-07-20 17:21:51 -0400

    init

---
+++ Source/ScrollableGizmos/GizmoPatch.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.Steam;
+using UnityEngine;
+using HarmonyLib;
+
+namespace ScrollableGizmos
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(GizmoGridDrawer), nameof(GizmoGridDrawer.DrawGizmoGrid))]
+    class GizmoPatch
+    {
+        public static Harmony harmony;
+        public static bool patched;
+        static GizmoPatch()
+        {
+            harmony = new Harmony("Blockdude.ScrollableGizmos");
+
+            if (GizmoSettings.enabled)
+            {
+                harmony.PatchAll();
+                patched = true;
+            }
+            else
+            {
+                patched = false;
+            }
+        }
+
+        // gizmo scroll
+        public static Vector2 scroll = new Vector2();
+
+        // some constants found in gizmo code
+        public const float scrollBarOffset = 16f;
+        public const float sideOffset = 147f;
+        public const float bottomOffset = 35f;
+        public const float buttonSize = 75f;
+
+        // lists for gizmo count
+        public static List<Gizmo> tmpAllGizmos = new List<Gizmo>();
+        public static List<List<Gizmo>> gizmoGroups = new List<List<Gizmo>>();
+
+        public static void CacheGizmos(IEnumerable<Gizmo> gizmos)
+        {
+            /*
+             * Begining of copied code so i can count gizmos
+             */
+            tmpAllGizmos.Clear();
+			gizmoGroups.Clear();
+			tmpAllGizmos.AddRange(gizmos);
+			for (int i = 0; i < tmpAllGizmos.Count; i++)
+			{
+				Gizmo gizmo = tmpAllGizmos[i];
+				bool flag = false;
+				for (int j = 0; j < gizmoGroups.Count; j++)
+				{
+					if (gizmoGroups[j][0].GroupsWith(gizmo))
+					{
+						flag = true;
+						gizmoGroups[j].Add(gizmo);
+						gizmoGroups[j][0].MergeWith(gizmo);
+						break;
+					}
+				}
+				if (!flag)
+				{
+					List<Gizmo> list = SimplePool<List<Gizmo>>.Get();
+					list.Add(gizmo);
+					gizmoGroups.Add(list);
+				}
+			}
+            /*
+             * End of copied code
+             */
+        }
+
+        public static int GetGizmoCount()
+        {
+            // calculate viewHeight
+            int gizmoCount = 0;
+            for (int i = 0; i < gizmoGroups.Count; i++)
+            {
+                if (gizmoGroups[i][0] != null && gizmoGroups[i][0].Visible)
+                    gizmoCount++;
+            }
+            return gizmoCount;
+        }
+
+        // patch to begining of gizmo drawing
+        public static void Prefix(ref IEnumerable<Gizmo> gizmos, ref float startX)
+        {
+            CacheGizmos(gizmos);
+
+            // calculate viewHeight
+            int gizmoCount = GetGizmoCount();
+
+            // height of full scroll view
+            float viewHeight;
+
+            // height of out rect for scroll view
+            float outHeight;
+
+            // total columns of gizmos
+            int gizmoColumnCount;
+
+            // total rows of gizmos
+            int gizmoRowCount;
+
+            // spacing that is set in the gizmo
+            float gizmoSpacing = GizmoGridDrawer.GizmoSpacing.x;
+
+            // calculate gizmo column count (unreliable solution for different width gizmos)
+            gizmoColumnCount = 0;
+            for (int i = 0; i < gizmoCount; i++)
+            {
+                gizmoColumnCount++;
+                if ((gizmoColumnCount * (buttonSize + gizmoSpacing)) > (UI.screenWidth - startX - (sideOffset - gizmoSpacing)))
+                {
+                    gizmoColumnCount--;
+                    break;
+                }
+            }
+
+            // calculate gizmo row count
+            gizmoRowCount = Mathf.CeilToInt((float)((float)gizmoCount / (float)gizmoColumnCount));
+
+            // calulate rect heights for scroll view
+            viewHeight = gizmoRowCount * (buttonSize + gizmoSpacing);
+            outHeight = Mathf.Min(viewHeight, GizmoSettings.outHeight);
+
+            // add 10 so top of gizmos are not cut off so it looks nice
+            viewHeight += 10f;
+            outHeight += 10f;
+
+            // create rects
+            Rect gizmoGroup = new Rect(0f, bottomOffset, Screen.width - scrollBarOffset - sideOffset, Screen.height);
+            Rect gizmoOut = new Rect(0f, Screen.height - outHeight - bottomOffset, Screen.width - sideOffset, outHeight);
+            Rect gizmoView = new Rect(0f, Screen.height - viewHeight, Screen.width - sideOffset - scrollBarOffset, viewHeight);
+
+            // start scroll
+            Widgets.BeginScrollView(gizmoOut, ref scroll, gizmoView, GizmoSettings.showScrollBar);
+            GUI.BeginGroup(gizmoGroup);
+        }
+
+        // patch to end of gizmo drawing
+        public static void Postfix()
+        {
+            GUI.EndGroup();
+            Widgets.EndScrollView();
+        }
+    }
+}
---
+++ Source/ScrollableGizmos/GizmoSettings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using UnityEngine;
+
+namespace ScrollableGizmos
+{
+    public class GizmoSettings : ModSettings
+    {
+        public static bool enabled = true;
+        public static bool showScrollBar = true;
+        public static float outHeight = 160;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref enabled, "enabled");
+            Scribe_Values.Look(ref showScrollBar, "showscrollbar");
+            Scribe_Values.Look(ref outHeight, "outheight");
+            base.ExposeData();
+
+            // patch and unpatch
+            if (enabled && !GizmoPatch.patched)
+            {
+                GizmoPatch.harmony.PatchAll();
+                GizmoPatch.patched = true;
+            }
+            else if (!enabled && GizmoPatch.patched)
+            {
+                GizmoPatch.harmony.UnpatchAll(GizmoPatch.harmony.Id);
+                GizmoPatch.patched = false;
+            }
+        }
+    }
+
+    public class GizmoSettingsMod : Mod
+    {
+        GizmoSettings settings;
+        string buffer;
+
+        public GizmoSettingsMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<GizmoSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("Enable Scrollable Gizmos", ref GizmoSettings.enabled);
+            listingStandard.CheckboxLabeled("Show Scrollbar", ref GizmoSettings.showScrollBar);
+            listingStandard.TextFieldNumericLabeled<float>("Scroll View height (increments of 80 look best)                                   ", ref GizmoSettings.outHeight, ref buffer);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Scrollable Gizmos";
+        }
+    }
+}
